55543c7bb0dc80788c8cfff6d633dc2354c0372f
Rahman Yusuf <danipart4@gmail.com>
2021-08-13 18:09:04 +0700

    added proper __init__.py

---
+++ discord/ext/music/__init__.py
@@ -0,0 +1,18 @@
+"""
+discord.ext.music
+~~~~~~~~~~~~~~~~~~
+
+Music extension module for [discord.py](https://github.com/Rapptz/discord.py)
+
+:copyright: (c) 2021 mansuf
+:license: MIT, see LICENSE for more details.
+"""
+
+__version__ = '0.0.1b'
+
+from .voice_client import *
+from .track import *
+from .playlist import *
+from .voice_source import *
+from .utils import *
+
