9818d847a54d7805cb19eabb898775e850139c64
Rahman Yusuf <danipart4@gmail.com>
2021-11-03 21:19:59 +0700

    add setup() to Equalizer class

--- discord/ext/music/equalizer/base.py
+++ discord/ext/music/equalizer/base.py
@@ -1,3 +1,4 @@
+import io
 
 __all__ = ('Equalizer',)
 
@@ -5,14 +6,23 @@ class Equalizer:
     """Represent a equalizer for converting audio.
 
     Sub-classes must implement this.
-
-    Parameters
-    ------------
-    stream: BufferedIOBase
-        Audio stream
     """
-    def __init__(self, stream):
-        pass
+    def __init__(self):
+        self.__stream__ = None # type: io.BufferedIOBase
+
+    def setup(self, stream):
+        """Initialize audio stream to Equalizer
+        
+        Warning
+        --------    
+        This is important to call it first, before equalizing audio.
+        
+        Parameters
+        -----------
+        stream: :class:`io.BufferedIOBase`
+            The audio stream that we want to equalize.
+        """
+        self.__stream__ = stream
 
     def read(self):
         """Read audio stream and return equalized audio data."""
