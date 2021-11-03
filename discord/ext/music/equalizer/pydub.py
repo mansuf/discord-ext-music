e60f8f0adcc0ab024878861fb1683bd9d7f2e5b6
Rahman Yusuf <danipart4@gmail.com>
2021-11-03 18:12:20 +0700

    added pydub equalizer

---
+++ discord/ext/music/equalizer/pydub.py
@@ -0,0 +1,246 @@
+import math
+
+from io import BufferedIOBase
+from typing import List, Union
+from discord.opus import _OpusStruct as OpusStruct
+from .base import Equalizer
+
+# Try to import eq function in pydub module
+try:
+    from pydub import AudioSegment
+    from pydub.scipy_effects import eq as equalizer
+    EQ_OK = True
+except ImportError:
+    EQ_OK = False
+
+class pydubError(Exception):
+    pass
+
+class pydubEqualizer(Equalizer):
+    """A pydub equalizer for Signed-PCM codec
+
+    The audio specifications must be 16-bit 48KHz
+
+    Warning
+    --------
+    You must have `scipy`_ and `pydub`_ installed, otherwise you will get error.
+
+    .. _scipy: https://pypi.org/project/scipy/
+    .. _pydub: https://pypi.org/project/pydub/
+
+    Parameters
+    -----------
+    stream: :class:`io.BufferedIOBase`
+        The audio stream that want to equalize.
+    freqs: Optional[List[:class:`dict`]]
+        a list containing dict, each dict has frequency (in Hz) and gain (in dB) inside it.
+        For example, [{"freq": 20, "gain": 20}, ...]
+        You cannot add same frequencys,
+        if you try to add it, it will raise :class:`pydubError`.
+    
+    Raises
+    -------
+    pydubError
+        pydub and scipy is not installed
+    """
+
+    def __init__(self, stream: BufferedIOBase, freqs: List[dict]=None):
+        if not EQ_OK:
+            raise pydubError('pydub and scipy need to be installed in order to use pydubEqualizer')
+        
+        self.stream = stream
+
+        if freqs is not None:
+            # Parse the frequencys
+            self._freqs = self._parse_freqs(freqs)
+        else:
+            self._freqs = {}
+
+        # PCM 16-bit 48000Hz Configurations
+        sample_width = 2
+        channels = 2
+        frame_rate = 48000
+
+        # For AudioSegment arguments
+        self._eq_args = {
+            "sample_width": sample_width,
+            "channels": channels,
+            "frame_rate": frame_rate,
+            "frame_width": channels * sample_width
+        }
+
+    def _determine_bandwidth(self, freqs):
+        if len(freqs) == 1:
+            return freqs[0]
+        max_freq = max(freqs)
+        min_freq = min(freqs)
+        return max_freq - min_freq
+
+    def _parse_freqs(self, freqs):
+        duplicate = []
+        new_freqs = {} # key: freq, value: gain
+        for data in freqs:
+            freq = data.get('freqs')
+            gain = data.get('gain')
+
+            if not freq:
+                raise ValueError('missing "freq" key in %s' % data)
+            
+            if not gain:
+                raise ValueError('missing "gain" key in %s' % data)
+            
+            self._check_freq(freq, gain)
+
+            if freq not in duplicate:
+                duplicate.append(freq)
+            else:
+                raise ValueError('frequency "%s" is more than one')
+
+            new_freqs[freq] = gain
+        return new_freqs
+
+    def _check_freq(self, freq=None, gain=None):
+        if freq and not isinstance(freq, int):
+            raise ValueError('freq "%s" is not integer type' % freq)
+        
+        if gain and (not isinstance(gain, float) or not isinstance(gain, int)):
+            raise ValueError('gain "%s" is not integer or float type')
+
+    def add_frequency(self, freq: int, gain: Union[int, float]):
+        """Add a frequency
+
+        Parameters
+        -----------
+        freq: :class:`int`
+            The frequency that want to add
+        gain: Union[:class:`int`, :class:`float`]
+            The gain frequency
+
+        Raises
+        -------
+        ValueError
+            given frequency is already exist
+        """
+        self._check_freq(freq, gain)
+
+        try:
+            self._freqs[freq]
+        except KeyError:
+            self._freqs.setdefault(freq, gain)
+        else:
+            raise ValueError('frequency "%s" is more than one, use set_gain() instead' % freq)
+
+    def remove_frequency(self, freq: int):
+        """Remove a frequency
+
+        Parameters
+        -----------
+        freq: :class:`int`
+            The frequency that want to add
+
+        Raises
+        -------
+        ValueError
+            given frequency is not exist
+        """
+        self._check_freq(freq)
+
+        try:
+            self._freqs.pop(freq)
+        except KeyError:
+            raise ValueError('frequency %s is not exist' % freq)
+    
+    def set_gain(self, freq: int, gain: int):
+        """
+        Set frequency gain in dB,
+
+        Parameters
+        -----------
+        freq: :class:`int`
+            The frequency want to increase the gain
+        gain: Union[:class:`int`, :class:`float`]
+            The value want to increase or lower
+
+        Raises
+        -------
+        ValueError
+            given frequency is not exist
+        """
+        self._check_freq(freq, gain)
+
+        if self._freqs.get(freq) is None:
+            raise ValueError('frequency %s is not exist' % freq)
+        self._freqs[freq] = gain
+
+    def read(self):
+        # The equalizer cannot convert audio if duration is too small (ex: 20ms)
+        # they will reproduce noisy sound.
+        # So the pydubEqualizer must read audio data for at least 1 second
+        # and then equalize it and move it to buffered equalized audio data.
+
+        # 1 second duration
+        _ = AudioSegment(self.stream.read(50 * OpusStruct.FRAME_SIZE))
+        segment = None
+
+        # Determine the bandwidth
+        bandwidth = self._determine_bandwidth(list(self._freqs))
+
+        # Equalize the audio
+        for frequency, gain in self._freqs.items():
+            try:
+                if segment is None:
+                    segment = equalizer(_, frequency, bandwidth, gain_dB=gain)
+                else:
+                    segment = equalizer(segment, frequency, bandwidth, gain_dB=gain)
+            except Exception:
+                raise pydubError('equalizing audio data failed')
+        
+        return segment.raw_data
+
+class pydubSubwooferEqualizer(Equalizer):
+    """
+    An easy to use :class:`pydubEqualizer` for subwoofer
+
+    The frequency is 60Hz.
+    
+    Parameters
+    -----------
+    volume: :class:`float`
+        Set initial volume as float percent.
+        For example, 0.5 for 50% and 1.75 for 175%.
+    """
+    def __init__(self, stream: BufferedIOBase, volume: float):
+        self.volume = volume
+        self._freq = 60
+        freqs = [{
+            "freq": self._freq,
+            "gain": self.volume
+        }]
+        self._eq = pydubEqualizer(stream, freqs)
+
+    @property
+    def volume(self) -> float:
+        """Optional[:class:`float`]: The subwoofer volume in float numbers
+        
+        This property can also be used to change the subwoofer volume.
+        """
+        return self._volume
+
+    @volume.setter
+    def volume(self, volume):
+        # Since given volume 0 or lower will raise error,
+        # try to redirectly changed it to lowest dB
+        if volume <= 0:
+            self._volume = -20.0
+            self.set_gain(self._volume)
+            return
+
+        # Adapted from https://github.com/Rapptz/discord.py/blob/master/discord/opus.py#L392
+        self._volume = 20 * math.log10(volume)
+        self.set_gain(self._volume)
+
+    def set_gain(self, dB: float):
+        """
+        Set frequency gain in dB.
+        """
+        self._eq.set_gain(self._freq, dB)
