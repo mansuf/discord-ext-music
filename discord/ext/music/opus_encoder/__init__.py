6a38bbb8bd706931e690b43a8b5bd970a9ca8ed9
Rahman Yusuf <danipart4@gmail.com>
2021-11-04 21:37:15 +0700

    added get_opus_encoder() to choose what opus encoder are available

--- discord/ext/music/opus_encoder/__init__.py
+++ discord/ext/music/opus_encoder/__init__.py
@@ -1,6 +1,26 @@
 from discord.opus import Encoder as NativeEncoder
-from .av import LibAVEncoder
+from .av import LibAVEncoder, AV_OK
+
+def get_opus_encoder(enc=None):
+    # List of available opus encoders
+    _opus_encoders = {
+        'native': NativeEncoder,
+        'av': LibAVEncoder
+    }
+
+    if enc:
+        try:
+            encoder = _opus_encoders[enc]
+        except KeyError:
+            raise ValueError('invalid opus encoder') from None
+        else:
+            return encoder
+    else:
+        if AV_OK:
+            return _opus_encoders['av']
+        else:
+            return _opus_encoders['native']
 
 __all__ = (
-    'NativeEncoder', 'LibAVEncoder'
+    'get_opus_encoder', 'NativeEncoder', 'LibAVEncoder'
 )
