2ecf171bdfa52fa6928a7da7652726938b368b45
Rahman Yusuf <danipart4@gmail.com>
2021-11-17 00:11:04 +0700

    bump version 0.3.0

--- discord/ext/music/__init__.py
+++ discord/ext/music/__init__.py
@@ -8,7 +8,7 @@ Music extension module for discord.py (https://github.com/Rapptz/discord.py)
 :license: MIT, see LICENSE for more details.
 """
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 from .voice_client import *
 from .track import *
